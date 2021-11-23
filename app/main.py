41db15df2a86d210430ba8b3c7257b4e40b88a63
Stepan Kuzin <stepankuzinmail@gmail.com>
2021-11-24 03:07:03 +0500

    Add example of swagger

--- app/main.py
+++ app/main.py
@@ -1,21 +1,66 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+from apispec_webframeworks.flask import FlaskPlugin
+from flask import Flask, jsonify
+from flask_restful import Api
 from flask_swagger_ui import get_swaggerui_blueprint
+from marshmallow import Schema, fields
 
 app = Flask(__name__)
 api = Api(app)
 
 
-class HelloWorld(Resource):
-    def get(self) -> dict:
-        return {"hello": "world"}
+spec = APISpec(
+    title="flask-api-swagger-doc",
+    version="1.0.0",
+    openapi_version="3.0.2",
+    plugins=[FlaskPlugin(), MarshmallowPlugin()],
+)
 
 
-api.add_resource(HelloWorld, "/hello")
+@app.route("/api/swagger.json")
+def create_swagger_spec() -> Flask.response_class:
+    return jsonify(spec.to_dict())
+
+
+class TodoResponseSchema(Schema):
+    id = fields.Int()
+    title = fields.Str()
+    status = fields.Boolean()
+
+
+class TodoListResponseSchema(Schema):
+    todo_list = fields.List(fields.Nested(TodoResponseSchema))
+
+
+@app.route("/todo")
+def todo() -> Flask.response_class:
+    """Get List of Todo
+    ---
+    get:
+        description: Get List of Todos
+        responses:
+            200:
+                description: Return a todo list
+                content:
+                    application/json:
+                        schema: TodoListResponseSchema
+    """
+
+    dummy_data = [
+        {"id": 1, "title": "Finish this task", "status": False},
+        {"id": 2, "title": "Finish that task", "status": True},
+    ]
+
+    return TodoListResponseSchema().dump({"todo_list": dummy_data})
+
+
+with app.test_request_context():
+    spec.path(view=todo)
 
 
 SWAGGER_URL = "/docs"  # URL for exposing Swagger UI (without trailing '/')
-API_URL = "http://petstore.swagger.io/v2/swagger.json"  # Our API url (can of course be a local resource)
+API_URL = "http://0.0.0.0:8000/api/swagger.json"  # Our API url (can of course be a local resource)
 
 # Call factory function to create our blueprint
 swaggerui_blueprint = get_swaggerui_blueprint(
