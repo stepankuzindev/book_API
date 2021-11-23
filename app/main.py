08f0975227879b75264a49186d63e484bfe697bc
Stepan Kuzin <stepankuzinmail@gmail.com>
2021-11-24 01:39:20 +0500

    Run Flask app

--- app/main.py
+++ app/main.py
@@ -1,15 +1,9 @@
-import sentry_sdk
-from fastapi import FastAPI
-from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
+from flask import Flask
 
-from api.v1.router import api_router
-from core.config import settings
+app = Flask(__name__)
 
 
-app = FastAPI(title=settings.PROJECT_NAME, openapi_url="/v1/openapi.json")
 
-if settings.SENTRY_DSN:
-    sentry_sdk.init(dsn=settings.SENTRY_DSN)
-    app.add_middleware(SentryAsgiMiddleware)
-
-app.include_router(api_router, prefix="/v1")
+@app.route('/hello')
+def hello():
+    return 'Hello World!!!'
