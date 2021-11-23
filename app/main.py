5457b9496fa9e71c12a6be973844287a1318c15f
Stepan Kuzin <stepankuzinmail@gmail.com>
2021-11-24 01:44:55 +0500

    fix main.py file

--- app/main.py
+++ app/main.py
@@ -3,12 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 
-
-@app.route('/hello')
-def hello():
-
-
-    return 'H' \
-           '' \
-           '' \
-           'ello World!!!'
+@app.route("/hello")
+def hello() -> str:
+    return "Hello World!!!"
