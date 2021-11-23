19fefd1d6c27036e9e27b354420e82bc5a068037
Stepan Kuzin <stepankuzinmail@gmail.com>
2021-11-24 02:04:35 +0500

    add swagger

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 
@@ -6,3 +7,24 @@ app = Flask(__name__)
 @app.route("/hello")
 def hello() -> str:
     return "Hello World!!!"
+
+
+SWAGGER_URL = "/docs"  # URL for exposing Swagger UI (without trailing '/')
+API_URL = "http://petstore.swagger.io/v2/swagger.json"  # Our API url (can of course be a local resource)
+
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={"app_name": "Test application"},  # Swagger UI config overrides
+    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    #    'clientId': "your-client-id",
+    #    'clientSecret': "your-client-secret-if-required",
+    #    'realm': "your-realms",
+    #    'appName': "your-app-name",
+    #    'scopeSeparator': " ",
+    #    'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
+
+app.register_blueprint(swaggerui_blueprint)
